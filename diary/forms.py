6c89cfabae08132f1ea19a580961a9c4ca41ed4b
kazumaru18 <k.tobe015@gmail.com>
2021-09-08 16:05:04 +0900

    forms.py（追加）

--- diary/forms.py
+++ diary/forms.py
@@ -10,4 +10,14 @@ class InquiryForm(forms.Form):
         super().__init__(*args, **kwargs)
 
         self.fields['name'].widget.attrs['class'] = 'form-control col-9'
+        self.fields['name'].widget.attrs['placeholder'] = 'お名前をここに入力してください。'
+
+        self.fields['email'].widget.attrs['class'] = 'form-control col-11'
+        self.fields['email'].widget.attrs['placeholder'] = 'メールアドレスをここに入力してください。'
+
+        self.fields['title'].widget.attrs['class'] = 'form-control col-11'
+        self.fields['title'].widget.attrs['placeholder'] = 'タイトルをここに入力してください。'
+
+        self.fields['message'].widget.attrs['class'] = 'form-control col-12'
+        self.fields['message'].widget.attrs['placeholder'] = 'メッセージをここに入力してください。'
         
